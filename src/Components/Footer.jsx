804309482f9b0d043b5d3f90def177153be4f1e4
ginofrancocba <ginofrancocba@gmail.com>
2023-10-21 00:52:12 -0300

    Agrego logo Navbar Footer e icon

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer>
         <p>Powered by SECTA solutions</p>
-        <img src="" alt='logo secta' />
+        <img src="/images/logo.png" alt='logo secta' />
         <p>Equipo 2</p>
     </footer>
   )
